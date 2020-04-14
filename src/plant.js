07eb65bf5242ffe24afcf41900da51642ad9ea17
Drake Wilcox <drakewilcox@gmail.com>
2020-04-14 14:01:36 -0700

    clean up code.
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>
    Co-authored-by: Matt Taylor <taylor.matt@protonmail.com>

--- src/plant.js
+++ src/plant.js
@@ -1,14 +1,5 @@
 
-// class Plant {
-//   constructor() {
-//     this.water = 0;
-//     this.soil = 0;
-//     this.light = 0;
-//   }
-// }
-
-
-const storeState = () => {
+export const storeState = () => {
   let currentState = { }; 
   return (stateChangeFunction) => {
     const newState = stateChangeFunction(currentState);
@@ -16,9 +7,9 @@ const storeState = () => {
       return newState;
   };
 };
+
 export const stateChanger = storeState();
-// REFACTOR to one curried function that can now change the property, state, and value.
-// This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
+
 const changeState = (prop) => {
   return (value) => {
     return (state) => ({
@@ -28,11 +19,12 @@ const changeState = (prop) => {
   };
 };
 
-
-// We create two functions using our function factory. We could easily create many more.
-// const plant1 = new Plant();
-// const plant2 = new Plant();
-// const plant3 = new Plant();
+export const newPlant = (name) => {
+  let plant = {
+    name
+  }
+  return {...plant, ...stateChanger}
+}
 
 const doubleFoodStore = () => {
   let currentState = {};
@@ -44,21 +36,10 @@ const doubleFoodStore = () => {
 }
 
 export const doubleFoodChanger = doubleFoodStore();
-// blueFood(plant1);
 
-// const feed = changeState("soil");
 export const blueFood = changeState("soil")(5);
 export const redFood = changeState("soil")(7);
 export const wateringCan = changeState("water")(5);
 export const sunnyDay = changeState("light")(7);
-
-// $(document).ready(function() {
-// // This function has side effects because we are using JQuery. 
-//   $('#feed').click(function() {
-//     const newState = stateChanger(blueFood);
-//     $('#soil-value').text(newState.soil);
-//     console.log(newState.soil);
-//   });
-// });
 
 
