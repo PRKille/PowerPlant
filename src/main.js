d9554c5bc122680b990ca6a069fa05f8e59b20d9
Drake Wilcox <drakewilcox@gmail.com>
2020-04-14 14:00:24 -0700

    refactor code to create and store a new plant variable.
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>
    Co-authored-by: Matt Taylor <taylor.matt@protonmail.com>

--- src/main.js
+++ src/main.js
@@ -1,36 +1,36 @@
-import { stateChanger, blueFood, wateringCan, sunnyDay, doubleFoodChanger } from './plant.js';
+import { storeState, wateringCan, sunnyDay, redFood } from './plant.js';
 import './scss/main.scss';
 import $ from 'jquery';
 
-$(document).ready(function() {
-  // This function has side effects because we are using JQuery. 
+const plant1 = storeState();
+const plant2 = storeState();
+
+$(document).ready(function() { 
   $('#feed').click(function() {
-    const newState = stateChanger(blueFood);
-    $('#soil-value').text(newState.soil);
+    const newPlant = plant1(redFood);
+    $('#soil-value').text(newPlant.soil);
   });
   $('#water').click(function() {
-    const newState = stateChanger(wateringCan);
-    $('#water-value').text(newState.water);
+    const newPlant = plant1(wateringCan);
+    $('#water-value').text(newPlant.water);
   });
   $('#sun').click(function() {
-    const newState = stateChanger(sunnyDay);
-    $('#sun-value').text(newState.light);
+    const newPlant = plant1(sunnyDay);
+    $('#sun-value').text(newPlant.light);
   });
 
   $('#feed2').click(function() {
-    // const newState = stateChanger(redFood)(wateringCan);
-    const newState2 = doubleFoodChanger(wateringCan);
-    // const newState2 = stateChanger(wateringCan);
-    $('#soil-value2').text(newState2.soil);
-    $('#water-value2').text(newState2.water);
+    const newPlant = plant2(redFood);
+    $('#soil-value2').text(newPlant.soil);
+    $('#water-value2').text(newPlant.water);
   });
   $('#water2').click(function() {
-    const newState2 = stateChanger(wateringCan);
-    $('#water-value2').text(newState2.water);
+    const newPlant = plant2(wateringCan);
+    $('#water-value2').text(newPlant.water);
   });
   $('#sun2').click(function() {
-    const newState2 = stateChanger(sunnyDay);
-    $('#sun-value2').text(newState2.light);
+    const newPlant = plant2(sunnyDay);
+    $('#sun-value2').text(newPlant.light);
   });
 });
 
